3c684a2a1b40c09e44269fcc2261862e22c98752
Pepeye <gaofolaju@gmail.com>
2017-03-06 00:20:58 +0000

    added me field to viewer

--- src/graph/viewer/type.js
+++ src/graph/viewer/type.js
@@ -3,7 +3,7 @@ import { globalIdField } from 'graphql-relay'
 import { nodeInterface, registerType } from '../node'
 
 // import queries for each node
-import { GraphQLUserQueries } from '../user'
+import { GraphQLUser, GraphQLUserQueries } from '../user'
 
 let rootFields = {
   ...GraphQLUserQueries
@@ -13,6 +13,12 @@ const viewerType = new GraphQLObjectType({
   name: 'Viewer',
   fields: () => ({
     id: globalIdField('Viewer'),
+    me: {
+      description: 'current logged in user',
+      type: GraphQLUser,
+      resolve: (root, args, { user }) => user
+      // resolve: (root, args, { user, loaders }) => 
+    },
     ...rootFields
   }),
   interfaces: [ nodeInterface ]
